8863879826dfd319af2f848b06c668411b188ec3
Abhishek Singh <84718593+abhsin96@users.noreply.github.com>
2022-11-22 21:24:42 +0530

    all route set

--- src/connector.js
+++ src/connector.js
@@ -1,18 +1,22 @@
-
 //const mongodb = require('mongodb');
+const dotenv = require("dotenv");
+dotenv.config();
+// const mongoURI = "mongodb://localhost:27017" + "/covidtally"
 
-const mongoURI = "mongodb://localhost:27017" + "/covidtally"
+let mongoose = require("mongoose");
+const { tallySchema } = require("./schema");
 
-let mongoose = require('mongoose');
-const { tallySchema } = require('./schema')
-
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => { console.log("connection established with mongodb server online"); })
-    .catch(err => {
-        console.log("error while connection", err)
-    });
-collection_connection = mongoose.model('covidtally', tallySchema)
-
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connection established with mongodb server online");
+  })
+  .catch((err) => {
+    console.log("error while connection", err);
+  });
+collection_connection = mongoose.model("covidtally", tallySchema);
 
 exports.connection = collection_connection;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,134 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
+const dotenv = require("dotenv");
+
+dotenv.config();
+const port = process.env.PORT || 1337;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const { connection } = require('./connector')
+const { connection } = require("./connector");
+const { data } = require("./data");
 
+app.get("/totalRecovered", async (req, res) => {
+  try {
+    const result = await connection.find();
 
+    let totalRecovered = 0;
 
+    result.forEach((element) => {
+      totalRecovered += element.recovered;
+    });
 
+    res.status(200).json({
+      data: {
+        _id: "total",
+        recovered: totalRecovered,
+      },
+    });
+  } catch (error) {
+    console.warn(error);
+  }
+});
 
+app.get("/totalActive", async (req, res) => {
+  try {
+    const result = await connection.find();
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+    let totalRecovered = 0;
+    let totalInfected = 0;
+
+    result.forEach((record) => {
+      totalRecovered += record.recovered;
+    });
+
+    result.forEach((record) => {
+      totalInfected += record.infected;
+    });
+
+    res.status(200).json({
+      data: {
+        _id: "total",
+        active: totalInfected - totalRecovered,
+      },
+    });
+  } catch (error) {
+    console.warn(error);
+  }
+});
+
+app.get("/totalDeath", async (req, res) => {
+  try {
+    const result = await connection.find();
+
+    let totalDeath = 0;
+
+    result.forEach((record) => {
+      totalDeath += record.death;
+    });
+
+    res.status(200).json({
+      data: {
+        _id: "total",
+        death: totalDeath,
+      },
+    });
+  } catch (error) {
+    console.warn(error);
+  }
+});
+
+app.get("/hotspotStates", async (req, res) => {
+  try {
+    const result = await connection.find();
+
+    let hotspotStates = [];
+    result.forEach((record) => {
+      const rate = parseFloat(
+        (record.infected - record.recovered) / record.infected
+      ).toFixed(5);
+      if (rate > 0.1) {
+        hotspotStates.push({
+          state: record.state,
+          rate,
+        });
+      }
+    });
+
+    res.status(200).json({
+      data: hotspotStates,
+    });
+  } catch (error) {
+    console.warn(error);
+  }
+});
+
+app.get("/healthyStates", async (req, res) => {
+  try {
+    const result = await connection.find();
+
+    let mortalityRate = [];
+
+    result.forEach((record) => {
+      const rate = parseFloat(record.death / record.infected).toFixed(5);
+      if (rate < 0.005) {
+        mortalityRate.push({
+          state: record.state,
+          mortality: rate,
+        });
+      }
+    });
+
+    res.status(200).json({
+      data: mortalityRate,
+    });
+  } catch (error) {
+    console.warn(error);
+  }
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
